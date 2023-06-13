0b1f048c8c11cd20f9b97e0bded4f070c32e79c7
AmrElda7i7 <amrfathymohamed7056@gmail.com>
2023-06-13 10:19:00 +0300

    bubble sort

---
+++ algorithms/sorting/BubbleSort.php
@@ -0,0 +1,43 @@
+<?php 
+$list=[7,81,8,21,21.23,14,1] ;
+print_r(BubbleSort($list)) ;
+print_r(BubbleSortOptimized($list)) ;
+
+// not optimized
+function BubbleSort($list)
+{
+    for($i=0;$i<sizeof($list);$i++)
+    {
+        for($j=0;$j<sizeof($list)-1 ;$j++) 
+        {
+            if($list[$j]>$list[$j+1] )
+            {
+                $swapped = $list[$j+1] ;
+                $list[$j+1] =$list[$j] ;
+                $list[$j] = $swapped ;
+            }
+        }
+    }
+    return $list ;
+}
+
+function BubbleSortOptimized($list)
+{
+    for($i=sizeof($list);$i>0;$i--)
+    {
+        $IsSwapped = false ;
+        for($j=0;$j<$i-1 ;$j++) 
+        {
+            if($list[$j]>$list[$j+1] )
+            {
+                $swapped = $list[$j+1] ;
+                $list[$j+1] =$list[$j] ;
+                $list[$j] = $swapped ;
+                $IsSwapped = true ;
+            }
+        }
+        if($IsSwapped==false) break ;
+
+    }
+    return $list ;
+}
